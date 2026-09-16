3de3bb6306b8a584f64cf1a862d65851afc08eb5
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-09-16 19:51:02 +0200

    Bump foolproof Android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "nl.hes.rebirthchecker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.4.0"
     }
 }
 dependencies {
