a2be64fb0b7d6a0c333c1bdeefd13c0de813b5da
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-09-16 19:11:41 +0200

    Align Android JVM targets

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,15 @@ plugins {
 android {
     namespace = "nl.hes.rebirthchecker"
     compileSdk = 35
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
     defaultConfig {
         applicationId = "nl.hes.rebirthchecker"
         minSdk = 26
